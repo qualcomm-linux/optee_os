d37d553862cf7cfb597de08ad5cae5ab4c1dae68
Sumit Garg <sumit.garg@oss.qualcomm.com>
2026-02-06 15:12:14 +0530

    pta: qcom_pas: Add support to bringup WPSS firmware
    WPSS is a co-processor to manage WiFi related functionality on Qcom
    platforms. Lets enable support to bring the co-processor firmware out
    of reset.
    
    Note that the firmware authentication is currently not added as that
    will come up next in future updates.
    
    Co-developed-by: Casey Connolly <casey.connolly@linaro.org>
    Signed-off-by: Casey Connolly <casey.connolly@linaro.org>
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

--- core/pta/qcom/pta_qcom_pas.c
+++ core/pta/qcom/pta_qcom_pas.c
@@ -6,10 +6,18 @@
 #include <initcall.h>
 #include <kernel/pseudo_ta.h>
 #include <kernel/user_ta.h>
+#include <platform_config.h>
 #include <pta_qcom_pas.h>
 #include <string.h>
 
+#include "pas.h"
+
 #define PTA_NAME	"pta.qcom.pas"
+
+static struct qcom_pas_data wpss_dsp_data = {
+	.base.pa = WPSS_BASE,
+	.clk_group = QCOM_CLKS_WPSS,
+};
 
 static TEE_Result qcom_pas_is_supported(uint32_t pt,
 					TEE_Param params[TEE_NUM_PARAMS])
@@ -19,14 +27,15 @@ static TEE_Result qcom_pas_is_supported(uint32_t pt,
 						TEE_PARAM_TYPE_NONE,
 						TEE_PARAM_TYPE_NONE);
 
-	(void)params;
-
 	if (pt != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	DMSG("invoked with pas_id: %d", params[0].value.a);
 
-	return TEE_ERROR_NOT_IMPLEMENTED;
+	if (params[0].value.a != PAS_ID_WPSS)
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	return TEE_SUCCESS;
 }
 
 static TEE_Result qcom_pas_capabilities(uint32_t pt,
@@ -55,14 +64,15 @@ static TEE_Result qcom_pas_init_image(uint32_t pt,
 						TEE_PARAM_TYPE_NONE,
 						TEE_PARAM_TYPE_NONE);
 
-	(void)params;
-
 	if (pt != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	DMSG("invoked with pas_id: %d", params[0].value.a);
 
-	return TEE_ERROR_NOT_IMPLEMENTED;
+	if (params[0].value.a != PAS_ID_WPSS)
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	return TEE_SUCCESS;
 }
 
 static TEE_Result qcom_pas_mem_setup(uint32_t pt,
@@ -73,14 +83,22 @@ static TEE_Result qcom_pas_mem_setup(uint32_t pt,
 						TEE_PARAM_TYPE_NONE,
 						TEE_PARAM_TYPE_NONE);
 
-	(void)params;
-
 	if (pt != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	DMSG("invoked with pas_id: %d", params[0].value.a);
 
-	return TEE_ERROR_NOT_IMPLEMENTED;
+	switch (params[0].value.a) {
+	case PAS_ID_WPSS:
+		wpss_dsp_data.fw_size = params[0].value.b;
+		wpss_dsp_data.fw_base = params[1].value.a;
+		wpss_dsp_data.fw_base |= ((paddr_t)params[1].value.b << 32);
+		break;
+	default:
+		return TEE_ERROR_NOT_SUPPORTED;
+	}
+
+	return TEE_SUCCESS;
 }
 
 static TEE_Result qcom_pas_get_resource_table(uint32_t pt,
@@ -90,14 +108,16 @@ static TEE_Result qcom_pas_get_resource_table(uint32_t pt,
 						TEE_PARAM_TYPE_MEMREF_INOUT,
 						TEE_PARAM_TYPE_NONE,
 						TEE_PARAM_TYPE_NONE);
-	(void)params;
 
 	if (pt != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	DMSG("invoked with pas_id: %d", params[0].value.a);
 
-	return TEE_ERROR_NOT_IMPLEMENTED;
+	if (params[0].value.a != PAS_ID_WPSS)
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	return TEE_SUCCESS;
 }
 
 static TEE_Result qcom_pas_auth_and_reset(uint32_t pt,
@@ -107,15 +127,31 @@ static TEE_Result qcom_pas_auth_and_reset(uint32_t pt,
 						TEE_PARAM_TYPE_VALUE_INPUT,
 						TEE_PARAM_TYPE_MEMREF_INPUT,
 						TEE_PARAM_TYPE_NONE);
-
-	(void)params;
+	TEE_Result res = TEE_SUCCESS;
 
 	if (pt != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	DMSG("invoked with pas_id: %d", params[0].value.a);
 
-	return TEE_ERROR_NOT_IMPLEMENTED;
+	switch (params[0].value.a) {
+	case PAS_ID_WPSS:
+		if (!wpss_dsp_data.fw_base)
+			return TEE_ERROR_NO_DATA;
+
+		res = qcom_clock_enable(wpss_dsp_data.clk_group);
+		if (res != TEE_SUCCESS) {
+			EMSG("Failed to enable clocks: %d", res);
+			return res;
+		}
+
+		wpss_dsp_start(&wpss_dsp_data);
+		break;
+	default:
+		return TEE_ERROR_NOT_SUPPORTED;
+	}
+
+	return TEE_SUCCESS;
 }
 
 static TEE_Result qcom_pas_set_remote_state(uint32_t pt,
