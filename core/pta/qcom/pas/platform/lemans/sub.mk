ldts/optee_os_qcom

pta: qcom: pas: lemans: add GP-DSP0/1 bring-up
Add PAS bring-up for the two general-purpose Hexagon DSPs on Lemans
(SA8775P): GP-DSP0 (TURINGGDSP, image id 39) and GP-DSP1 (TURINGGDSP1,
image id 40). This follows the same architecture as the existing
CDSP0/1, LPASS and IRIS subsystems.

Like LPASS, the Q6 PLL and core RCG are configured in the clock-setup
stage (gpdsp_setup) before the boot FSM; the core is released by
BOOT_CORE_START in gpdspN_fw_start, so the post-boot processor-enable
step is a no-op. Like CDSP, a full subsystem reset (gpdsp_reset_processor,
mirroring Reset_TURINGGDSPProcessor) is used for the tear-down/shutdown
path and before cold bring-up; unlike the Turing NSP it omits the
NSPAUX/ALT_RESET retention handling and checks two master-idle ports
(IL0 and IL1).

All register offsets, PLL values, IOMMU resource tables and PAS image
IDs are taken from the reference TZ sources (ClockPIL.c,
HALclkPLLSettings.h, HALclkHWIO.h, ACSmmuStaticConfig.c gpdsp{0,1}_q6_elf[],
tz_syscall_pub.h).

VASPACE required an increase.

Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

core/pta/qcom/pas/platform/lemans/sub.mk
@@ -1,3 +1,3 @@
-srcs-y += entry.c cdsp0.c cdsp1.c lpass.c iris.c
+srcs-y += entry.c cdsp0.c cdsp1.c lpass.c iris.c gpdsp0.c gpdsp1.c
 incdirs-y += .
 incdirs-y += ../

qualcomm-linux/optee_os

core/pta/qcom/pas/platform/lemans/sub.mk
@@ -1,3 +1,3 @@
-srcs-y += subsys.c cdsp0.c cdsp1.c lpass.c iris.c
+srcs-y += subsys.c cdsp0.c cdsp1.c lpass.c iris.c gpdsp0.c gpdsp1.c
 incdirs-y += .
 incdirs-y += ../