ldts/optee_os_qcom

pta: qcom: pas: lemans: add IRIS video-codec bring-up
Add the IRIS video-codec PAS driver for lemans, mirroring the kodiak
venus driver (which already targets IRIS hardware). The lemans IRIS
register layout is identical: WRAPPER_TZ at IRIS+0xc0000 with the same
XTSS_SW_RESET / FW / CPA / NONPIX offsets.

Bring-up programs the FW/CPA/NONPIX windows and clears the XTSS soft
reset; shutdown asserts it. Like venus, IRIS needs no clock-group
handling and has no resource table. The video PAS id is 9
(TZ_PIL_AUTH_VIDEO_PROC).

Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

core/pta/qcom/pas/platform/lemans/sub.mk
@@ -1,3 +1,3 @@
-srcs-y += entry.c cdsp0.c cdsp1.c lpass.c
+srcs-y += entry.c cdsp0.c cdsp1.c lpass.c iris.c
 incdirs-y += .
 incdirs-y += ../

qualcomm-linux/optee_os

core/pta/qcom/pas/platform/lemans/sub.mk
@@ -1,3 +1,3 @@
-srcs-y += subsys.c cdsp0.c cdsp1.c lpass.c
+srcs-y += subsys.c cdsp0.c cdsp1.c lpass.c iris.c
 incdirs-y += .
 incdirs-y += ../