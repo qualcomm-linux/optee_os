66f43269eb252029a630e59f0955056743c7c7c8
Harikrishna <hart@qti.qualcomm.com>
2026-07-29 14:38:24 +0530

    plat-qcom: ipq96xx: enable CDSP PAS support
    Enable the PAS PTA and Qualcomm clock driver on ipq96xx, add the CDSP
    (Turing/NSP) register window bases (Turing, GCC, MPM2, TCSR), and
    register the PAS pseudo TA as an in-tree early TA.
    
    Testing:
    Built for PLATFORM_FLAVOR=ipq96xx with aarch64-linux-gnu-; the CDSP PAS
    and clock objects link into the image and the PAS early TA is signed.
    
    Signed-off-by: Harikrishna <hart@qti.qualcomm.com>

--- core/arch/arm/plat-qcom/bobcat/ipq96xx/target_config.h
+++ core/arch/arm/plat-qcom/bobcat/ipq96xx/target_config.h
@@ -22,6 +22,22 @@
 #define QCOM_WDT_TMR_BASE		UL(0x0F411000)
 #define QCOM_WDT_BARK_INT_ID		UL(0x36)
 
+/* CDSP (Turing/NSP) PAS register windows. */
+#define GCC_BASE			UL(0x01800000)
+#define GCC_SIZE			UL(0x00080000)
+
+#define TURING_BASE			UL(0x26000000)
+#define TURING_SIZE			UL(0x00400000)
+
+#define MPM2_MPM_BASE			UL(0x004A0000)
+#define MPM2_MPM_SIZE			UL(0x00010000)
+
+#define TCSR_SPARE_BASE			UL(0x01959000)
+#define TCSR_SPARE_SIZE			UL(0x00001000)
+
+#define CDSP_TCSR_BASE			UL(0x01966000)
+#define CDSP_TCSR_SIZE			UL(0x00001000)
+
 /* eMMC (SDCC) is the ICE-backed storage controller on ipq96xx */
 #define ICE_LUT_KEYS			SDCC_ICE_LUT_KEYS
 #define ICE_LUT_KEYS_SIZE		SDCC_ICE_LUT_KEYS_SIZE
