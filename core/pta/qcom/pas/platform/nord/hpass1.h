732095e9714f253da6e9312023586b69cde9d132
“Deepak <kudee@qti.qualcomm.com>
2026-08-25 01:54:18 +0530

    pta: qcom: pas: nord: register HPASS0-2 and NSPSS0-3 PAS subsystems
    Register the PAS bring-up for Nord's subsystems HPASS & NSPSS.
    - hpass0-2: program the HPASS reset-EVB and TCSR EVB-select
      registers for each HPASS instance from its own base offset and
      TCSR stride index.
    - nspss0-3: program the NSPSS reset-EVB register for each
      CDSP instance.
    - subsys.c: register all subsystems (HPASS0-2, NSPSS0-3)
      with their PAS IDs, base/size and clock groups.
    
    Signed-off-by: “Deepak <kudee@qti.qualcomm.com>

---
+++ core/pta/qcom/pas/platform/nord/hpass1.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef _HPASS1_H_
+#define _HPASS1_H_
+
+#include "pas_subsys.h"
+
+extern const struct qcom_pas_ops hpass1_ops;
+
+#endif /* _HPASS1_H_ */
---
+++ core/pta/qcom/pas/platform/nord/sub.mk
@@ -0,0 +1,3 @@
+srcs-y += subsys.c hpass0.c hpass1.c hpass2.c nspss0.c nspss1.c nspss2.c nspss3.c
+incdirs-y += .
+incdirs-y += ../
