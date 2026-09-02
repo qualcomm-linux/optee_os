178534177350c2fee75b7272317a5db4c4654204
Anil Veshala Veshala <anil.veshala@oss.qualcomm.com>
2026-09-01 23:48:25 -0700

    drivers: qcom: add GENI I2C driver and enable i2c config
    Add a Qualcomm I2C geni driver implementing i2c_ctrl_ops for a GENI
    Serial Engine in FIFO transfer mode, driven by polling rather than
    interrupts. Enabled CFG_QCOM_GENI_I2C in lemans platform, and add
    the corresponding qup config settings in qup_i2c_config[] table.
    
    Signed-off-by: Anil Veshala Veshala <anil.veshala@oss.qualcomm.com>

---
+++ core/drivers/i2c/qcom/platform/lemans/sub.mk
@@ -0,0 +1,1 @@
+srcs-$(CFG_QCOM_GENI_I2C) += qcom_geni_i2c_config.c
---
+++ core/drivers/i2c/qcom/sub.mk
@@ -0,0 +1,4 @@
+srcs-$(CFG_QCOM_GENI_I2C) += qcom_geni_i2c.c
+srcs-$(CFG_QUP_I2C_TEST) += qcom_geni_i2c_test.c
+
+subdirs-$(CFG_QCOM_GENI_I2C) += platform
