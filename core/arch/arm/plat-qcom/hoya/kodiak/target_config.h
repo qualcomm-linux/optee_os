2f019b31ef3f836a69875c70c05848b249c57da4
mrehman <mrehman@qti.qualcomm.com>
2026-09-02 18:26:44 +0530

    plat-qcom: adjust CSRNG register definitions
    Use platform-specific CSRNG base addresses and offsets
    to avoid redefining common RNG configuration while
    preserving the effective register addresses.
    
    Signed-off-by: mrehman <mrehman@qti.qualcomm.com>

--- core/arch/arm/plat-qcom/hoya/kodiak/target_config.h
+++ core/arch/arm/plat-qcom/hoya/kodiak/target_config.h
@@ -40,9 +40,9 @@
 #define IRIS_SIZE			ULL(0x00200000)
 
 /* Configuration Registers for RNG */
-#define QCOM_RNG_REG_BASE      UL(0x010C0000)
+#define QCOM_RNG_REG_BASE      UL(0x010D0000)
 #define QCOM_RNG_REG_SIZE      UL(0x00020000)
-#define QCOM_RNG_DATA_OUT      0x11000
-#define QCOM_RNG_STATUS        0x11004
+#define QCOM_RNG_DATA_OUT      0x1000
+#define QCOM_RNG_STATUS        0x1004
 
 #endif /* TARGET_CONFIG_H */
--- core/arch/arm/plat-qcom/hoya/lemans/target_config.h
+++ core/arch/arm/plat-qcom/hoya/lemans/target_config.h
@@ -93,10 +93,10 @@
 #define RPMH_PDC_GPDSP1_SIZE		UL(0x00002000)
 
 /* Configuration Registers for RNG */
-#define QCOM_RNG_REG_BASE      UL(0x010C0000)
+#define QCOM_RNG_REG_BASE      UL(0x010D0000)
 #define QCOM_RNG_REG_SIZE      UL(0x00020000)
-#define QCOM_RNG_DATA_OUT      0x11000
-#define QCOM_RNG_STATUS        0x11004
+#define QCOM_RNG_DATA_OUT      0x1000
+#define QCOM_RNG_STATUS        0x1004
 
 #define TITAN_SS_BASE			UL(0x0ac00000)
 #define TITAN_SS_SIZE			UL(0x00200000)
