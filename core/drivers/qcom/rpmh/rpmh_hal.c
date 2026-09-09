qualcomm-linux/optee_os

commit b6ff325df1a5a7f913697f3813bd24c4b38d5efb

drivers: qcom: rpmh: add RPMH client driver
Add Resource Power Manager Hardened (RPMH) client driver for
Qualcomm platforms.

RPMH provides a communication interface to the Always-On Processor
for power management operations. It uses Trigger Command Sets (TCS)
to send resource state requests (e.g., voltage levels, clock states)
to the AOP, which then programs the hardware accordingly.

The driver implements:
- Client handle management for Secure driver ID
- Active set command submission (sleep/wake not needed for Secure DRV)
- TCS hardware sequencing and completion barriers
- Resource command tracking and caching
- Hardware abstraction layer for register access

This driver is essential for controlling voltage rails during
QFPROM fuse provisioning, which requires precise voltage levels
(0.95V on MX rail) to safely program one-time-programmable fuses.

Signed-off-by: Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
Reviewed-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

core/drivers/qcom/rpmh/rpmh_hal.c
@@ -1,0 +1,292 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#include <assert.h>
+#include <io.h>
+#include <util.h>
+
+#include "rpmh_hal.h"
+
+static struct {
+	vaddr_t rsc_base;
+	vaddr_t drv_base[RSC_DRV_MAX];
+} rsc_base_addrs;
+
+static inline vaddr_t get_drv_base(enum rsc_drv_id drv_id)
+{
+	return (drv_id >= RSC_DRV_MAX) ? 0 : rsc_base_addrs.drv_base[drv_id];
+}
+
+static inline vaddr_t get_tcs_base(vaddr_t base, uint32_t tcs_id)
+{
+	return base + TCS_BASE_OFFSET + (tcs_id * TCS_STRIDE);
+}
+
+static inline uint32_t hal_read32(vaddr_t addr)
+{
+	return io_read32(addr);
+}
+
+static inline void hal_write32(vaddr_t addr, uint32_t val)
+{
+	io_write32(addr, val);
+}
+
+enum hal_status hal_rpmh_init(vaddr_t rsc_base)
+{
+	if (!rsc_base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	rsc_base_addrs.rsc_base = rsc_base;
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_register_drv(enum rsc_drv_id drv_id)
+{
+	if (drv_id >= RSC_DRV_MAX)
+		return HAL_STATUS_INVALID_PARAM;
+
+	rsc_base_addrs.drv_base[drv_id] = rsc_base_addrs.rsc_base +
+					  (drv_id * DRV_STRIDE);
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_read_config(enum rsc_drv_id drv_id,
+				     uint32_t *tcs, uint32_t *cmds)
+{
+	uint32_t config = 0;
+	vaddr_t base = 0;
+
+	if (!tcs || !cmds)
+		return HAL_STATUS_INVALID_PARAM;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	config = hal_read32(base + RSC_DRV_TCS_CONFIG);
+	*tcs = (config >> 0) & 0xFF;
+	*cmds = (config >> 8) & 0xFF;
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_convert_to_amc(enum rsc_drv_id drv_id,
+					uint32_t tcs_id)
+{
+	vaddr_t tcs_base = 0;
+	uint32_t control = 0;
+	vaddr_t base = 0;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	tcs_base = get_tcs_base(base, tcs_id);
+	control = hal_read32(tcs_base + TCS_CONTROL_OFFSET);
+	control |= TCS_CONTROL_AMC_MODE_EN;
+	hal_write32(tcs_base + TCS_CONTROL_OFFSET, control);
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_convert_to_tcs(enum rsc_drv_id drv_id,
+					uint32_t tcs_id)
+{
+	vaddr_t tcs_base = 0;
+	uint32_t control = 0;
+	vaddr_t base = 0;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	tcs_base = get_tcs_base(base, tcs_id);
+	control = hal_read32(tcs_base + TCS_CONTROL_OFFSET);
+	control &= ~TCS_CONTROL_AMC_MODE_EN;
+	hal_write32(tcs_base + TCS_CONTROL_OFFSET, control);
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_enable_amc_status(enum rsc_drv_id drv_id,
+					   uint32_t tcs_id)
+{
+	uint32_t enable = 0;
+	vaddr_t base = 0;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	enable = hal_read32(base + RSC_DRV_IRQ_ENABLE);
+	enable |= BIT(tcs_id);
+	hal_write32(base + RSC_DRV_IRQ_ENABLE, enable);
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_clear_amc_status(enum rsc_drv_id drv_id,
+					  uint32_t tcs_id)
+{
+	vaddr_t base = 0;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	hal_write32(base + RSC_DRV_IRQ_CLEAR, BIT(tcs_id));
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_is_tcs_idle(enum rsc_drv_id drv_id,
+				     uint32_t tcs_id, bool *idle)
+{
+	vaddr_t tcs_base = 0;
+	uint32_t status = 0;
+	vaddr_t base = 0;
+
+	if (!idle)
+		return HAL_STATUS_INVALID_PARAM;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	tcs_base = get_tcs_base(base, tcs_id);
+	status = hal_read32(tcs_base + TCS_STATUS_OFFSET);
+	*idle = (status & TCS_STATUS_CONTROLLER_IDLE) != 0;
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_get_amc_status(enum rsc_drv_id drv_id,
+					uint32_t tcs_id,
+					bool *finished)
+{
+	uint32_t status = 0;
+	vaddr_t base = 0;
+
+	if (!finished)
+		return HAL_STATUS_INVALID_PARAM;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	status = hal_read32(base + RSC_DRV_IRQ_STATUS);
+	*finished = (status & BIT(tcs_id)) != 0;
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_send_tcs(enum rsc_drv_id drv_id,
+				  uint32_t tcs_id,
+				  uint32_t enable_mask)
+{
+	vaddr_t tcs_base = 0;
+	uint32_t control = 0;
+	vaddr_t base = 0;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	tcs_base = get_tcs_base(base, tcs_id);
+	hal_write32(tcs_base + TCS_CMD_ENABLE_OFFSET, enable_mask);
+	control = hal_read32(tcs_base + TCS_CONTROL_OFFSET);
+	control |= TCS_CONTROL_AMC_MODE_EN;
+	control &= ~TCS_CONTROL_AMC_MODE_TRIGGER;
+	hal_write32(tcs_base + TCS_CONTROL_OFFSET, control);
+	control |= TCS_CONTROL_AMC_MODE_TRIGGER;
+	hal_write32(tcs_base + TCS_CONTROL_OFFSET, control);
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_write_cmd(enum rsc_drv_id drv_id,
+				   uint32_t tcs_id, uint32_t cmd_idx,
+				   uint32_t addr, uint32_t data,
+				   bool completion)
+{
+	uint32_t slave_id = 0;
+	uint32_t addr_reg = 0;
+	vaddr_t cmd_base = 0;
+	vaddr_t tcs_base = 0;
+	uint32_t offset = 0;
+	uint32_t msgid = 0;
+	vaddr_t base = 0;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	tcs_base = get_tcs_base(base, tcs_id);
+	cmd_base = tcs_base + TCS_CMD_BASE_OFFSET + (cmd_idx * TCS_CMD_STRIDE);
+
+	msgid = 0;
+	msgid |= SHIFT_U32(0, MSGID_READ_OR_WRITE_SHIFT);
+	msgid |= SHIFT_U32((completion ? 1 : 0), MSGID_RES_REQ_SHIFT);
+	msgid |= SHIFT_U32(1, MSGID_MSG_LENGTH_SHIFT);
+
+	slave_id = (addr >> 16) & 0x7;
+	offset = addr & 0xFFFF;
+	addr_reg = SHIFT_U32(slave_id, ADDR_SLV_ID_SHIFT) |
+		   SHIFT_U32(offset, ADDR_OFFSET_SHIFT);
+
+	hal_write32(cmd_base + TCS_CMDn_MSGID_OFFSET, msgid);
+	hal_write32(cmd_base + TCS_CMDn_ADDR_OFFSET, addr_reg);
+	hal_write32(cmd_base + TCS_CMDn_DATA_OFFSET, data);
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_update_epcb_timeout(enum rsc_drv_id drv_id,
+					     uint32_t threshold)
+{
+	uint32_t val = 0;
+	vaddr_t base = 0;
+
+	if (drv_id != RSC_DRV_SECURE)
+		return HAL_STATUS_INVALID_PARAM;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	val = hal_read32(base + RSC_DRV_ERROR_IRQ_ENABLE);
+	val &= ~EPCB_TIMEOUT_THRESHOLD_MASK;
+	val |= SHIFT_U32((threshold & EPCB_TIMEOUT_THRESHOLD_MASK),
+			 EPCB_TIMEOUT_THRESHOLD_SHIFT);
+	hal_write32(base + RSC_DRV_ERROR_IRQ_ENABLE, val);
+
+	return HAL_STATUS_SUCCESS;
+}
+
+enum hal_status hal_rpmh_toggle_epcb_timeout(enum rsc_drv_id drv_id,
+					     bool enable)
+{
+	uint32_t val = 0;
+	vaddr_t base = 0;
+
+	if (drv_id != RSC_DRV_SECURE)
+		return HAL_STATUS_INVALID_PARAM;
+
+	base = get_drv_base(drv_id);
+	if (!base)
+		return HAL_STATUS_INVALID_PARAM;
+
+	val = hal_read32(base + RSC_DRV_ERROR_IRQ_ENABLE);
+	if (enable)
+		val |= EPCB_TIMEOUT_IRQ_EN_MASK;
+	else
+		val &= ~EPCB_TIMEOUT_IRQ_EN_MASK;
+	hal_write32(base + RSC_DRV_ERROR_IRQ_ENABLE, val);
+
+	return HAL_STATUS_SUCCESS;
+}

core/drivers/qcom/rpmh/rpmh_hal.h
@@ -1,0 +1,88 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef __RPMH_HAL_H__
+#define __RPMH_HAL_H__
+
+#include <drivers/qcom/rpmh/rpmh_client.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <util.h>
+
+enum hal_status {
+	HAL_STATUS_SUCCESS = 0,
+	HAL_STATUS_ERROR = 1,
+	HAL_STATUS_INVALID_PARAM = 2,
+};
+
+#define DRV_STRIDE			0x10000
+#define RSC_DRV_IRQ_ENABLE		0x0d00
+#define RSC_DRV_IRQ_STATUS		0x0d04
+#define RSC_DRV_IRQ_CLEAR		0x0d08
+
+#define RSC_DRV_TCS_CONFIG		0x0C
+#define TCS_BASE_OFFSET			0x0D10  /* CMD_WAIT_FOR_CMPL base */
+#define TCS_STRIDE			0x2A0
+
+#define TCS_CONTROL_OFFSET		0x04
+#define TCS_STATUS_OFFSET		0x08
+#define TCS_CMD_ENABLE_OFFSET		0x0C
+
+#define TCS_CMD_BASE_OFFSET		0x20
+#define TCS_CMDn_MSGID_OFFSET		0x00
+#define TCS_CMDn_ADDR_OFFSET		0x04
+#define TCS_CMDn_DATA_OFFSET		0x08
+#define TCS_CMD_STRIDE			0x14
+
+#define TCS_CONTROL_AMC_MODE_TRIGGER	BIT(24)
+#define TCS_CONTROL_AMC_MODE_EN		BIT(16)
+
+#define TCS_STATUS_CONTROLLER_IDLE	BIT(0)
+
+#define RSC_DRV_ERROR_IRQ_STATUS	0xD0
+#define RSC_DRV_ERROR_IRQ_ENABLE	0xD8
+#define RSC_DRV_ERROR_IRQ_CLEAR		0xD4
+
+#define EPCB_TIMEOUT_IRQ_EN_MASK	BIT(20)
+#define EPCB_TIMEOUT_THRESHOLD_SHIFT	0x0
+#define EPCB_TIMEOUT_THRESHOLD_MASK	0xFFFF
+
+#define MSGID_READ_OR_WRITE_SHIFT	0x10
+#define MSGID_RES_REQ_SHIFT		0x8
+#define MSGID_MSG_LENGTH_SHIFT		0x0
+
+#define ADDR_SLV_ID_SHIFT		0x10
+#define ADDR_OFFSET_SHIFT		0x0
+
+enum hal_status hal_rpmh_init(vaddr_t rsc_base);
+enum hal_status hal_rpmh_register_drv(enum rsc_drv_id drv_id);
+enum hal_status hal_rpmh_read_config(enum rsc_drv_id drv_id,
+				     uint32_t *tcs, uint32_t *cmds);
+enum hal_status hal_rpmh_convert_to_amc(enum rsc_drv_id drv_id,
+					uint32_t tcs_id);
+enum hal_status hal_rpmh_convert_to_tcs(enum rsc_drv_id drv_id,
+					uint32_t tcs_id);
+enum hal_status hal_rpmh_enable_amc_status(enum rsc_drv_id drv_id,
+					   uint32_t tcs_id);
+enum hal_status hal_rpmh_clear_amc_status(enum rsc_drv_id drv_id,
+					  uint32_t tcs_id);
+enum hal_status hal_rpmh_is_tcs_idle(enum rsc_drv_id drv_id,
+				     uint32_t tcs_id, bool *idle);
+enum hal_status hal_rpmh_get_amc_status(enum rsc_drv_id drv_id,
+					uint32_t tcs_id,
+					bool *finished);
+enum hal_status hal_rpmh_send_tcs(enum rsc_drv_id drv_id,
+				  uint32_t tcs_id,
+				  uint32_t enable_mask);
+enum hal_status hal_rpmh_write_cmd(enum rsc_drv_id drv_id,
+				   uint32_t tcs_id, uint32_t cmd_idx,
+				   uint32_t addr, uint32_t data,
+				   bool completion);
+enum hal_status hal_rpmh_update_epcb_timeout(enum rsc_drv_id drv_id,
+					     uint32_t threshold);
+enum hal_status hal_rpmh_toggle_epcb_timeout(enum rsc_drv_id drv_id,
+					     bool enable);
+
+#endif /* __RPMH_HAL_H__ */

core/drivers/qcom/rpmh/rpmh_client.c
@@ -1,0 +1,370 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#include <assert.h>
+#include <compiler.h>
+#include <drivers/qcom/cmd_db/cmd_db.h>
+#include <drivers/qcom/rpmh/rpmh_client.h>
+#include <initcall.h>
+#include <inttypes.h>
+#include <io.h>
+#include <kernel/delay.h>
+#include <kernel/mutex.h>
+#include <kernel/panic.h>
+#include <mm/core_memprot.h>
+#include <mm/core_mmu.h>
+#include <platform_config.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/queue.h>
+#include <util.h>
+
+#include "rpmh_hal.h"
+#include "rpmh_resource_commands.h"
+#include "rpmh_tcs.h"
+
+register_phys_mem_pgdir(MEM_AREA_IO_NSEC, AOP_MSG_RAM_BASE,
+			CORE_MMU_PGDIR_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, RPMH_BASE_ADDR,
+			CORE_MMU_PGDIR_SIZE);
+
+struct rpmh_driver_data {
+	vaddr_t rsc_base;
+	struct client_queue *queue;
+	struct mutex lock; /* Protects driver state and client operations */
+};
+
+struct client {
+	enum rsc_drv_id drv_id;
+	const char *name;
+	uint32_t req_id;
+	uint32_t oldest_req_id;
+	uint32_t num_reqs_in_progress;
+	struct explicit_cmd_set *cmds;
+	SLIST_ENTRY(client) link;
+};
+
+struct explicit_cmd_set {
+	uint32_t in_use;
+	uint32_t total;
+	struct cmd_set_internal *sets;
+};
+
+struct cmd_set_internal {
+	enum rpmh_set set;
+	uint32_t count;
+	uint32_t dependency_bmsk;
+	struct rpmh_command commands[RPMH_MAX_TCS_SIZE];
+};
+
+SLIST_HEAD(client_list, client);
+
+struct client_queue {
+	struct client_list handles;
+};
+
+#define AOP_BOOT_COOKIE		0xA0C00C1E
+#define MSG_RAM_SECTION_SIZE	0x10000
+
+struct aop_msg_ram_dict {
+	uint32_t boot_cookie_offset;
+	uint32_t sleep_stats_offset;
+	uint32_t reserved_addrs[14];
+};
+
+static struct rpmh_driver_data driver_state = {
+	.lock = MUTEX_INITIALIZER,
+};
+
+static bool tcs_is_idle(enum rsc_drv_id drv_id, uint32_t timeout_us)
+{
+	uint64_t timer = timeout_init_us(timeout_us);
+
+	while (!rpmh_tcs_is_amc_free(drv_id)) {
+		if (timeout_elapsed(timer))
+			return false;
+		udelay(1);
+	}
+
+	return true;
+}
+
+static uint32_t issue_cmd_set_internal(struct client *client,
+				       struct cmd_set_internal *set)
+{
+	struct rpmh_resource_command temp_rc = { };
+	struct rpmh_resource_command *rc = NULL;
+	uint32_t enable_mask = 0;
+	bool completion = false;
+	uint32_t req_id = 0;
+	bool dirty = false;
+	uint32_t addr = 0;
+	uint32_t data = 0;
+	uint32_t i = 0;
+
+	for (i = 0; i < set->count; i++) {
+		addr = set->commands[i].address;
+		data = set->commands[i].data;
+		completion = set->commands[i].completion;
+
+		rc = rpmh_find_resource_command(addr);
+		if (!rc) {
+			rpmh_resource_command_init(&temp_rc, addr);
+			rc = &temp_rc;
+		}
+
+		if (rpmh_resource_command_update(rc, set->set, data,
+						 completion, client->drv_id,
+						 false))
+			dirty = true;
+	}
+
+	if (!dirty || set->set != RPMH_SET_ACTIVE)
+		return 0;
+
+	if (!tcs_is_idle(client->drv_id, 1000)) {
+		EMSG("TCS idle timeout for drv %"PRIu32, client->drv_id);
+		return 0;
+	}
+
+	req_id = ++client->req_id;
+	client->num_reqs_in_progress++;
+
+	for (i = 0; i < set->count; i++) {
+		if (hal_rpmh_write_cmd(client->drv_id, 0, i,
+				       set->commands[i].address,
+				       set->commands[i].data,
+				       set->commands[i].completion) !=
+		    HAL_STATUS_SUCCESS) {
+			EMSG("Failed to write cmd %"PRIu32" addr 0x%"PRIx32, i,
+			     set->commands[i].address);
+			return 0;
+		}
+	}
+
+	enable_mask = GENMASK_32(set->count - 1, 0);
+	if (hal_rpmh_send_tcs(client->drv_id, 0, enable_mask) !=
+	    HAL_STATUS_SUCCESS) {
+		EMSG("Failed to send TCS for drv %"PRIu32, client->drv_id);
+		return 0;
+	}
+
+	return req_id;
+}
+
+static struct client *find_client(enum rsc_drv_id drv_id,
+				  const char *name)
+{
+	struct client *client = NULL;
+
+	SLIST_FOREACH(client, &driver_state.queue->handles, link) {
+		if (client->drv_id == drv_id && !strcmp(client->name, name))
+			return client;
+	}
+
+	return NULL;
+}
+
+static struct client *create_client(enum rsc_drv_id drv_id,
+				    const char *name, bool explicit)
+{
+	struct client *client = find_client(drv_id, name);
+
+	if (client)
+		return client;
+
+	client = calloc(1, sizeof(struct client));
+	if (!client)
+		return NULL;
+
+	client->drv_id = drv_id;
+	client->name = name;
+	client->req_id = 0;
+	client->oldest_req_id = 0;
+	client->num_reqs_in_progress = 0;
+
+	if (explicit) {
+		client->cmds = calloc(1, sizeof(struct explicit_cmd_set));
+		if (!client->cmds) {
+			free(client);
+			return NULL;
+		}
+	}
+
+	SLIST_INSERT_HEAD(&driver_state.queue->handles, client, link);
+	return client;
+}
+
+static bool wait_for_cmd(struct client *client, uint32_t req_id,
+			 bool wait_all)
+{
+	uint64_t timer = timeout_init_us(10000);
+	bool cmd_complete = false;
+
+	if (req_id == 0)
+		return true;
+
+	while (!timeout_elapsed(timer)) {
+		if (wait_all)
+			cmd_complete = (client->num_reqs_in_progress == 0);
+		else if (hal_rpmh_get_amc_status(client->drv_id, 0,
+						 &cmd_complete))
+			cmd_complete = false;
+
+		if (cmd_complete)
+			break;
+
+		udelay(1);
+	}
+
+	if (cmd_complete && client->num_reqs_in_progress > 0)
+		client->num_reqs_in_progress--;
+
+	return cmd_complete;
+}
+
+static TEE_Result check_aop_init(void)
+{
+	uint64_t timer = timeout_init_us(100000);
+	struct aop_msg_ram_dict *dict = NULL;
+	vaddr_t cookie_addr = 0;
+	vaddr_t dict_addr = 0;
+	uint32_t cookie = 0;
+	vaddr_t base = 0;
+
+	base = (vaddr_t)phys_to_virt(AOP_MSG_RAM_BASE, MEM_AREA_IO_NSEC,
+				     AOP_MSG_RAM_SIZE);
+	if (!base) {
+		EMSG("Failed to get VA for AOP message RAM at PA 0x%lx",
+		     (unsigned long)AOP_MSG_RAM_BASE);
+		return TEE_ERROR_GENERIC;
+	}
+
+	dict_addr = base + AOP_MSG_RAM_SIZE - MSG_RAM_SECTION_SIZE;
+	dict = (struct aop_msg_ram_dict *)dict_addr;
+	cookie_addr = base + dict->boot_cookie_offset;
+
+	while (cookie != AOP_BOOT_COOKIE) {
+		cookie = io_read32(cookie_addr);
+		if (cookie == AOP_BOOT_COOKIE)
+			break;
+
+		if (timeout_elapsed(timer)) {
+			EMSG("AOP boot timeout after 100ms");
+			return TEE_ERROR_BUSY;
+		}
+
+		udelay(1);
+	}
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result rpmh_client_init(void)
+{
+	TEE_Result res = TEE_SUCCESS;
+
+	res = check_aop_init();
+	if (res != TEE_SUCCESS) {
+		EMSG("AOP initialization check failed");
+		goto err_panic;
+	}
+
+	driver_state.rsc_base = (vaddr_t)phys_to_virt(RPMH_BASE_ADDR,
+						      MEM_AREA_IO_SEC,
+						      RPMH_RSC_SIZE);
+	if (!driver_state.rsc_base) {
+		EMSG("Failed to get VA for RSC base at PA 0x%lx",
+		     (unsigned long)RPMH_BASE_ADDR);
+		goto err_panic;
+	}
+
+	driver_state.queue = calloc(1, sizeof(struct client_queue));
+	if (!driver_state.queue) {
+		EMSG("Failed to allocate client queue");
+		goto err_panic;
+	}
+
+	SLIST_INIT(&driver_state.queue->handles);
+
+	if (hal_rpmh_init(driver_state.rsc_base) != HAL_STATUS_SUCCESS ||
+	    rpmh_tcs_init() != TEE_SUCCESS) {
+		EMSG("Failed to initialize HAL/TCS");
+		goto err_panic;
+	}
+
+	return TEE_SUCCESS;
+
+err_panic:
+	panic("RPMH driver initialization failed");
+}
+
+struct rpmh_client *rpmh_create_handle(enum rsc_drv_id drv_id,
+				       const char *name)
+{
+	struct rpmh_client *handle = NULL;
+
+	if (!name || drv_id != RSC_DRV_SECURE)
+		return NULL;
+
+	mutex_lock(&driver_state.lock);
+	handle = (struct rpmh_client *)create_client(drv_id, name, false);
+	mutex_unlock(&driver_state.lock);
+
+	return handle;
+}
+
+TEE_Result rpmh_send_command(struct rpmh_client *handle,
+			     enum rpmh_set set, bool completion,
+			     uint32_t address, uint32_t data,
+			     uint32_t *req_id)
+{
+	struct client *client = (struct client *)handle;
+	struct cmd_set_internal cmd_set = { };
+	TEE_Result res = TEE_SUCCESS;
+	uint32_t id = 0;
+
+	if (!client || !req_id)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	*req_id = 0;
+	mutex_lock(&driver_state.lock);
+
+	cmd_set.commands[0].address = address;
+	cmd_set.commands[0].data = data;
+	cmd_set.commands[0].completion = completion;
+	cmd_set.set = set;
+	cmd_set.count = 1;
+	cmd_set.dependency_bmsk = 0;
+
+	id = issue_cmd_set_internal(client, &cmd_set);
+	if (id == 0) {
+		res = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	if (!wait_for_cmd(client, id, false)) {
+		res = TEE_ERROR_BUSY;
+		goto out;
+	}
+
+	*req_id = id;
+
+out:
+	mutex_unlock(&driver_state.lock);
+	return res;
+}
+
+void rpmh_barrier_single(struct rpmh_client *handle, uint32_t req_id)
+{
+	struct client *client = (struct client *)handle;
+
+	if (!client)
+		return;
+
+	wait_for_cmd(client, req_id, false);
+}
+
+early_init(rpmh_client_init);

core/drivers/qcom/rpmh/rpmh_drv_config.c
@@ -1,0 +1,45 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#include <stddef.h>
+
+#include "rpmh_drv_config.h"
+#include "rpmh_target_config.h"
+
+/*
+ * Common RPMh driver configuration describing TCS layout and limits
+ * (AMC/SLEEP/WAKE mapping, command capacity, driver IDs, wake latencies)
+ */
+
+static const struct tcs_config tcs_config_tz = {
+	.amcs = RPMH_TCS_SLEEP - RPMH_TCS_AMC,
+	.sleep_start = RPMH_TCS_SLEEP,
+	.wake_start = RPMH_TCS_WAKE,
+};
+
+static const struct drv_config_data optee_config_data = {
+	.drvs_count = 1,
+	.init_clks_count = 0,
+	.init_clks = NULL,
+	.sleep_clks_count = 0,
+	.sleep_clks = NULL,
+
+	.drvs = (struct drv_config[]) {
+		{
+			.drv_id = RSC_DRV_SECURE,
+			.hw_drv = RSC_DRV_SECURE,
+			.wake_set_latency = 0x7080, /* 1.5ms in 19.2MHz */
+			.tcs_offset = 0,
+			.tcs = RPMH_TCS_MAX,
+			.cmds = RPMH_MAX_CMDS_PER_TCS,
+			.modes_count = 1,
+			.modes = (const struct tcs_config *[]) {
+				&tcs_config_tz,
+			}
+		}
+	}
+};
+
+const struct drv_config_data *const g_drv_config_data = &optee_config_data;

core/drivers/qcom/rpmh/rpmh_drv_config.h
@@ -1,0 +1,42 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef __RPMH_DRV_CONFIG_H__
+#define __RPMH_DRV_CONFIG_H__
+
+#include <drivers/qcom/rpmh/rpmh_client.h>
+#include <stdint.h>
+
+/* RPMH DRV Configuration */
+
+struct tcs_config {
+	uint32_t amcs;          /* Number of AMCs (active TCS) */
+	uint32_t sleep_start;   /* Index where sleep TCS start */
+	uint32_t wake_start;    /* Index where wake TCS start */
+};
+
+struct drv_config {
+	enum rsc_drv_id drv_id;
+	enum rsc_drv_id hw_drv;
+	uint32_t wake_set_latency;
+	uint32_t tcs_offset;
+	uint32_t tcs;
+	uint32_t cmds;
+	uint32_t modes_count;
+	const struct tcs_config **modes;
+};
+
+struct drv_config_data {
+	uint32_t drvs_count;
+	uint32_t init_clks_count;
+	char **init_clks;
+	uint32_t sleep_clks_count;
+	char **sleep_clks;
+	struct drv_config *drvs;
+};
+
+extern const struct drv_config_data *const g_drv_config_data;
+
+#endif /* __RPMH_DRV_CONFIG_H__ */

core/drivers/qcom/rpmh/rpmh_resource_commands.c
@@ -1,0 +1,189 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#include <assert.h>
+#include <drivers/qcom/cmd_db/cmd_db.h>
+#include <malloc.h>
+#include <string.h>
+
+#include "rpmh_drv_config.h"
+#include "rpmh_resource_commands.h"
+#include "rpmh_tcs.h"
+
+static struct rpmh_resource_command *resources;
+static uint32_t resources_count;
+
+static void
+reconcile_vote_state(struct rpmh_resource_command __maybe_unused *rc,
+		     struct drv_votes *drv, bool completion)
+{
+	struct rpmh_vote *active = NULL;
+	struct rpmh_vote *sleep = NULL;
+	struct rpmh_vote *wake = NULL;
+
+	if (!drv)
+		return;
+
+	sleep = &drv->local_votes[RPMH_SET_SLEEP];
+	active = &drv->local_votes[RPMH_SET_ACTIVE];
+	wake = &drv->local_votes[RPMH_SET_WAKE];
+
+	if (active->valid &&
+	    (!drv->vote_at_rpmh.valid ||
+	     active->data != drv->vote_at_rpmh.data ||
+	     (completion && !active->completion))) {
+		active->dirty = RPMH_RC_DIRTY;
+	} else {
+		active->dirty = RPMH_RC_CLEAN;
+	}
+
+	if (sleep->dirty != RPMH_RC_CLEAN_EXPLICIT_VOTE) {
+		if (sleep->valid &&
+		    (!active->valid ||
+		     (active->valid && active->data != sleep->data))) {
+			sleep->dirty = RPMH_RC_DIRTY;
+		} else {
+			sleep->dirty = RPMH_RC_CLEAN;
+		}
+	}
+
+	if (wake->dirty != RPMH_RC_CLEAN_EXPLICIT_VOTE) {
+		if (wake->valid &&
+		    ((active->valid && wake->data != active->data) ||
+		     (sleep->valid && wake->data != sleep->data))) {
+			wake->dirty = RPMH_RC_DIRTY;
+		} else if (!wake->valid &&
+			   (active->valid && sleep->valid) &&
+			   active->data != sleep->data &&
+			   sleep->dirty != RPMH_RC_CLEAN_EXPLICIT_VOTE) {
+			wake->dirty = RPMH_RC_DIRTY_USE_ACTIVE;
+		} else {
+			wake->dirty = RPMH_RC_CLEAN;
+		}
+	}
+}
+
+void rpmh_resource_command_init(struct rpmh_resource_command *rc,
+				uint32_t address)
+{
+	if (!rc)
+		return;
+
+	memset(rc, 0, sizeof(*rc));
+	rc->address = address;
+}
+
+static struct drv_votes *
+rpmh_resource_command_get_voter(struct rpmh_resource_command *rc,
+				enum rsc_drv_id drv_id)
+{
+	struct drv_votes *prev = NULL;
+	struct drv_votes *drv = NULL;
+
+	if (!rc)
+		return NULL;
+
+	drv = rc->drv_votes;
+
+	while (drv) {
+		if (drv->drv_id == drv_id)
+			break;
+		prev = drv;
+		drv = drv->next;
+	}
+
+	if (!drv) {
+		const struct drv_config *config = NULL;
+		TEE_Result res = TEE_SUCCESS;
+		uint32_t drv_index = 0;
+		uint32_t priority = 0;
+
+		drv = calloc(1, sizeof(struct drv_votes));
+		if (!drv)
+			return NULL;
+
+		drv->drv_id = drv_id;
+
+		if (rpmh_tcs_find_drv_index(drv_id, &drv_index) !=
+		    TEE_SUCCESS) {
+			free(drv);
+			return NULL;
+		}
+
+		config = &g_drv_config_data->drvs[drv_index];
+
+		res = cmd_db_get_priority(rc->address,
+					  (uint8_t)config->hw_drv,
+					  &priority);
+		if (res != TEE_SUCCESS) {
+			EMSG("Failed to query priority for addr 0x%"PRIx32,
+			     rc->address);
+			free(drv);
+			return NULL;
+		}
+
+		drv->priority = (enum rpmh_rc_priority)priority;
+
+		if (!rc->drv_votes)
+			rc->drv_votes = drv;
+		else if (prev)
+			prev->next = drv;
+		else
+			return NULL;
+	}
+
+	return drv;
+}
+
+struct rpmh_resource_command *rpmh_find_resource_command(uint32_t address)
+{
+	uint32_t high = resources_count - 1;
+	uint32_t low = 0;
+
+	if (resources_count == 0)
+		return NULL;
+
+	while (high >= low) {
+		uint32_t mid = (low + high) / 2;
+		uint32_t addr = 0;
+
+		if (high >= resources_count)
+			break;
+
+		addr = resources[mid].address;
+
+		if (addr < address)
+			low = mid + 1;
+		else if (addr > address)
+			high = mid - 1;
+		else
+			return &resources[mid];
+	}
+
+	return NULL;
+}
+
+bool rpmh_resource_command_update(struct rpmh_resource_command *rc,
+				  enum rpmh_set set, uint32_t data,
+				  bool completion,
+				  enum rsc_drv_id drv_id,
+				  bool explicit_cmd)
+{
+	struct drv_votes *drv = NULL;
+
+	drv = rpmh_resource_command_get_voter(rc, drv_id);
+	drv->local_votes[set].data = data;
+	drv->local_votes[set].valid = true;
+
+	if (explicit_cmd && set != RPMH_SET_ACTIVE)
+		drv->local_votes[set].dirty = RPMH_RC_CLEAN_EXPLICIT_VOTE;
+
+	reconcile_vote_state(rc, drv,
+			     completion && set == RPMH_SET_ACTIVE);
+
+	drv->local_votes[set].completion = completion;
+
+	return (drv->local_votes[set].dirty > RPMH_RC_LAST_CLEAN);
+}

core/drivers/qcom/rpmh/rpmh_resource_commands.h
@@ -1,0 +1,71 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef __RPMH_RESOURCE_COMMANDS_H__
+#define __RPMH_RESOURCE_COMMANDS_H__
+
+#include <drivers/qcom/rpmh/rpmh_client.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <tee_api_types.h>
+
+enum rpmh_rc_state {
+	RPMH_RC_STATE_IDLE = 0,
+	RPMH_RC_STATE_IN_PROGRESS,
+	RPMH_RC_STATE_MAX
+};
+
+enum rpmh_rc_dirty {
+	RPMH_RC_CLEAN = 0,
+	RPMH_RC_CLEAN_EXPLICIT_VOTE = 1,
+	RPMH_RC_LAST_CLEAN = 1,
+	RPMH_RC_DIRTY = 2,
+	RPMH_RC_DIRTY_MOL = 3,
+	RPMH_RC_DIRTY_USE_ACTIVE = 4,
+	RPMH_RC_DIRTY_MAX
+};
+
+struct rpmh_vote {
+	uint32_t data;
+	bool valid;
+	bool completion;
+	enum rpmh_rc_dirty dirty;
+};
+
+enum rpmh_rc_priority {
+	RPMH_RC_PRIORITY_USE_CASE = 0,
+	RPMH_RC_PRIORITY_SUBSYSTEM,
+	RPMH_RC_PRIORITY_MAX
+};
+
+struct drv_votes {
+	enum rsc_drv_id drv_id;
+	enum rpmh_rc_state state;
+	enum rpmh_rc_priority priority;
+	struct rpmh_vote vote_at_rpmh;
+	struct rpmh_vote local_votes[RPMH_NUM_SETS];
+	struct drv_votes *next;
+};
+
+struct rpmh_resource_command {
+	uint32_t address;
+	struct drv_votes *drv_votes;
+};
+
+/* Initialize resource command */
+void rpmh_resource_command_init(struct rpmh_resource_command *rc,
+				uint32_t address);
+
+/* Update resource command vote, returns true if set is dirty */
+bool rpmh_resource_command_update(struct rpmh_resource_command *rc,
+				  enum rpmh_set set,
+				  uint32_t data, bool completion,
+				  enum rsc_drv_id drv_id,
+				  bool explicit_cmd);
+
+/* Find resource command by address */
+struct rpmh_resource_command *rpmh_find_resource_command(uint32_t address);
+
+#endif /* __RPMH_RESOURCE_COMMANDS_H__ */

commit c05f6fef0aa359d8a21677e620003e63c2b295fc

drivers: qcom: rpmh: fix MSGID MSG_LENGTH
The RPMh command MSGID encodes a MSG_LENGTH field describing the
payload length, in bytes, of the command. This field was hardcoded
to 1 which is leading to unpredictable behavior on the
AOP side (including the command never being acknowledged, causing timeouts).

Changing it to 8 (the correct length for the single 32-bit data
word every RPMh command carries).
Using MSGID_WRITE since it's a write command.

Fixes: b6ff325df1 (drivers: qcom: rpmh: add RPMH client driver)
Signed-off-by: Shivam Sanjay <shivsanj@qti.qualcomm.com>
Reviewed-by: Dinesh Choudhary <idinesh@qti.qualcomm.com>
Reviewed-by: Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>

core/drivers/qcom/rpmh/rpmh_hal.c
@@ -230,9 +230,9 @@ enum hal_status hal_rpmh_write_cmd(enum rsc_drv_id drv_id,
 	cmd_base = tcs_base + TCS_CMD_BASE_OFFSET + (cmd_idx * TCS_CMD_STRIDE);
 
 	msgid = 0;
-	msgid |= SHIFT_U32(0, MSGID_READ_OR_WRITE_SHIFT);
+	msgid |= SHIFT_U32(MSGID_WRITE, MSGID_READ_OR_WRITE_SHIFT);
 	msgid |= SHIFT_U32((completion ? 1 : 0), MSGID_RES_REQ_SHIFT);
-	msgid |= SHIFT_U32(1, MSGID_MSG_LENGTH_SHIFT);
+	msgid |= SHIFT_U32(MSGID_MSG_LENGTH_VALUE, MSGID_MSG_LENGTH_SHIFT);
 
 	slave_id = (addr >> 16) & 0x7;
 	offset = addr & 0xFFFF;

core/drivers/qcom/rpmh/rpmh_hal.h
@@ -53,6 +53,12 @@ enum hal_status {
 #define MSGID_RES_REQ_SHIFT		0x8
 #define MSGID_MSG_LENGTH_SHIFT		0x0
 
+#define MSGID_READ			0x0
+#define MSGID_WRITE			0x1
+
+/* Each RPMh command transfers a single 32-bit word (encoded as 8 bytes). */
+#define MSGID_MSG_LENGTH_VALUE		0x8
+
 #define ADDR_SLV_ID_SHIFT		0x10
 #define ADDR_OFFSET_SHIFT		0x0
 